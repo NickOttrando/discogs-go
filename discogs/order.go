a752331e37268d10771beba8524b4ed7ac80b0fa
Nick Ottrando <nottrando@gmail.com>
2019-08-25 11:57:29 -0700

    wrap up orders except auth check, specs for auth

--- discogs/order.go
+++ discogs/order.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+type OrderMessage struct {
+	Subject   string `json:"subject"`
+	Message   string `json:"message"`
+	To        User   `json:"to"`
+	Order     Order  `json:"order"`
+	Timestamp string `json:"timestamp"`
+}
+
 type Order struct {
 	ID                     string    `json:"id"`
 	Status                 string    `json:"status"`
@@ -28,6 +36,11 @@ type OrdersResponse struct {
 	Pagination
 }
 
+type OrderMessagesResponse struct {
+	Messages []OrderMessage `json:"messages"`
+	Pagination
+}
+
 // todo: raise error unless Client is authenticated?
 func (c *Client) GetOrder(orderID string) (out *Order, err error) {
 	err = c.get(fmt.Sprintf("marketplace/orders/%s", orderID), nil, &out)
@@ -44,5 +57,18 @@ func (c *Client) GetOrders(opts *ListOptions) (out *OrdersResponse, err error) {
 		return nil, err
 	}
 	err = c.get("marketplace/orders", &fmtedOpts, &out)
+	return
+}
+
+// todo: raise error if not auth
+func (c *Client) GetOrderMessages(orderID string, opts *ListOptions) (out *OrderMessagesResponse, err error) {
+	var fmtedOpts *ListOptionsFmted
+	if opts != nil {
+		fmtedOpts, err = opts.Format()
+	}
+	if err != nil {
+		return nil, err
+	}
+	err = c.get(fmt.Sprintf("marketplace/orders/%s/messages", orderID), &fmtedOpts, &out)
 	return
 }
--- discogs/order_test.go
+++ discogs/order_test.go
@@ -19,3 +19,9 @@ func TestGetOrdersNoAuth(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, userOrders)
 }
+
+func TestGetOrderMessagesNoAuth(t *testing.T) {
+	orderMessages, err := testClient.GetOrderMessages(testOrderID, nil)
+	assert.NotNil(t, err)
+	assert.Nil(t, orderMessages)
+}
