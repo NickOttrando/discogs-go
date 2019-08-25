9e62da012cfd6218f618efd2c14762a27f11c618
Nick Ottrando <nottrando@gmail.com>
2019-08-25 13:47:14 -0700

    update main script

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/NickOttrando/discogs-go/discogs"
+	"github.com/NickOttrando/discogs-go/discogs"
 )
 
 func main() {
-    clientOptions := discogs.ClientOptions{
-        UserAgent: os.Getenv("DISCOGS_USER_AGENT"),
-        Token:     os.Getenv("DISCOGS_TOKEN"),
-    }
+	clientOptions := discogs.ClientOptions{
+		UserAgent: os.Getenv("DISCOGS_USER_AGENT"),
+		Token:     os.Getenv("DISCOGS_TOKEN"),
+	}
 
-    discogsClient, err := discogs.NewClient(clientOptions)
-    if err != nil {
-        panic(err)
-    }
+	discogsClient, err := discogs.NewClient(clientOptions)
+	if err != nil {
+		panic(err)
+	}
 
-    // fetch artist profile
-    artist, err := discogsClient.GetArtist(1713695)
-    fmt.Println(artist.Name)
+	// fetch artist profile
+	artist, err := discogsClient.GetArtist(1713695)
+	fmt.Println(artist.Name)
 
-    // fetch artist releases, see how many pages there are
-    artistReleases, err := discogsClient.GetArtistReleases(1713695, nil)
-    fmt.Println(artistReleases.Releases[0].Title)
-    fmt.Println(artistReleases.Pages)
+	// fetch artist releases, see how many pages there are
+	artistReleases, err := discogsClient.GetArtistReleases(1713695, nil)
+	fmt.Println(artistReleases.Releases[0].Title)
+	fmt.Println(artistReleases.Pages)
 
-    moreReleases, err := discogsClient.GetArtistReleases(1713695, &discogs.ListOptions{Page: 2, Sort: "year", PerPage: 5})
-    fmt.Println(moreReleases.Releases[0].Title)
-    fmt.Println(moreReleases.Page)
+	moreReleases, err := discogsClient.GetArtistReleases(1713695, &discogs.ListOptions{Page: 2, Sort: "year", PerPage: 5})
+	fmt.Println(moreReleases.Releases[0].Title)
+	fmt.Println(moreReleases.Page)
+
+	// fetch label and their releases
+	label, err := discogsClient.GetLabel(23528)
+	fmt.Println(label.Name)
+
+	labelReleases, err := discogsClient.GetLabelReleases(23528, nil)
+	fmt.Println(labelReleases.Releases[0].Title)
 }
